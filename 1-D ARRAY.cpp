9320263d8bc262abe0a370cdce20059275aa8236
patriki44 <patrickruai469@gmail.com>
2024-10-17 22:37:22 +0300

    Add files via upload

---
+++ 1-D ARRAY.cpp
@@ -0,0 +1,12 @@
+//Arrays in c -1-D
+#include<stdio.h>
+int main(){
+	int i;
+	int marks[5]={50,90,60,0,40};
+	for(i=0;i<5;i++){
+		printf("marks[%d]-%d \n " ,i,marks[i]);
+	}
+	return 0;
+	
+}
+
---
+++ 2-D ARRAY.cpp
@@ -0,0 +1,12 @@
+//Arrays in 2-D
+#include<stdio.h>
+int main(){
+	int i,j;
+	int marks[2][3]={{50,90,60},{40,70,40}};
+	for(i=0;i<2;i++){
+		for(j=0;j<3;j++){
+			printf("marks[%d][%d]-%d \n",i,j,marks[i][j]);
+		}
+	}
+	return 0;
+}
